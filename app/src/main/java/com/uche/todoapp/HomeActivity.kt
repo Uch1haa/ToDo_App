0d486890df220c0abdd2ccc1c3cb011c7dd98901
willxy21 <williamsuche77@gmail.com>
2021-06-22 14:23:51 +0100

    homepage

---
+++ app/src/main/java/com/uche/todoapp/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.uche.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var usernameDisplay : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        usernameDisplay = findViewById(R.id.usernameDisplay)
+        usernameDisplay.text = "THE FUTURE IS HERE"
+    }
+}
